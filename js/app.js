cca92102356dc535a4d750616de8f573f3c35d54
En1o <andrey.p0209@gmail.com>
2023-06-30 19:12:10 +0300

    starting create task card and task render

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+// Task Kard
+const arrTasks = [];
 let taskTitle = 'Title', taskText = 'Test text';
 
 const taskCard = {
@@ -6,3 +9,27 @@ const taskCard = {
     isCompleted: false
 }
 
+function pushTask(task){
+    arrTasks.push(task);
+}
+
+//delete
+pushTask(taskCard)
+pushTask(taskCard)
+pushTask(taskCard)
+
+console.log(arrTasks)
+
+//Task List generate
+const ulList = document.createElement('ul'),
+      ulListElement = document.createElement('li'),
+      spanListElement = document.createElement('span');
+
+function generateTaskCard(el){
+    spanListElement.innerText = taskCard.taskTitle
+}
+generateTaskCard(taskCard)
+
+ulListElement.insertAdjacentElement('afterbegin', spanListElement)
+ulList.insertAdjacentElement('afterbegin', ulListElement)
+document.body.insertAdjacentElement('afterbegin', ulList)
